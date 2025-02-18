79715d12ed9c2c3839ab9f1d07a020c9dfea0435
wolxXx <devops@wolxxx.de>
2025-02-18 17:50:51 +0100

    upgrade to php 8.4, introducing opcache, minor tidy ups

--- html/index.php
+++ html/index.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
+#ini_set('opcache.enable', 0);
+ini_set(option: 'opcache.max_file_size', value: 1000);
+ini_set(option: 'opcache.file_cache_only', value: 1);
+ini_set(option: 'opcache.file_cache', value: __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . '.opcache');
 date_default_timezone_set("Europe/Berlin");
 
 \Application\Manager::Factory();
