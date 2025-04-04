6f4ff0ac90d187b07b81b2c14516b84f26c5e01e
wolxXx <devops@wolxxx.de>
2025-04-04 23:37:57 +0200

    Add modals for viewing deployment details in groups
    Introduced modal dialogs to display detailed deployment information for each group. Added functionality to handle opening, closing, and backdrop interactions for the modals, along with a reload timeout reset. Improved UI by including 'show' buttons and refined styles for better organization.

--- app/views/action/index.php
+++ app/views/action/index.php
@@ -5,22 +5,106 @@ declare(strict_types=1);
  */
 
 ?>
+<dialog style="min-width: 400px;">
+    <button autofocus>Close</button>
+    <p>This modal dialog has a groovy backdrop!</p>
+</dialog>
+
+<script>
+    let timeoutHandler = null;
+    let modalOpen = false;
+    function initReload() {
+        timeoutHandler = window.setTimeout(function() {
+            location.reload();
+        }, 20000);
+    }
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        initReload()
+        const dialog = document.querySelector("dialog");
+
+        document.addEventListener('click', function (event) {
+            if(true === modalOpen) {
+                event.stopPropagation();
+                event.preventDefault();
+                dialog.close()
+                initReload()
+                modalOpen = false
+
+                return false
+            }
+        })
+
+        document.querySelectorAll('.showDeployments').forEach(function (element) {
+            element.addEventListener('click', function (event) {
+                event.stopPropagation();
+                event.preventDefault();
+                let closest = element.closest('.group');
+                let querySelector = closest.querySelector('.modalContent');
+                let innerHTML = querySelector.innerHTML;
+                dialog.innerHTML = innerHTML;
+                document.querySelector('::backdrop')?.addEventListener('click', function () {
+                    dialog.close()
+                    initReload()
+                    modalOpen = false
+
+                })
+                dialog.querySelector('.closeButton').addEventListener('click', function () {
+                    dialog.close()
+                    initReload()
+                    modalOpen = false
+                })
+                dialog.showModal();
+                window.clearTimeout(timeoutHandler);
+                modalOpen = true
+
+                return false;
+            })
+        })
+
+    });
+</script>
 
 <? foreach ($view->groups as $group): ?>
 
     <?
         $count = 0;
     ?>
-    <div style="display: flex; flex-direction: column; width: 15%; border: 1px solid white; padding: 0.2%; word-wrap: anywhere;">
+    <div style="display: flex; flex-direction: column; width: 15%; border: 1px solid white; padding: 0.2%; word-wrap: anywhere;" class="group">
         <div style="text-align: left; font-weight: bold; font-size: 1.3em; padding-bottom: 2px;">
             <?= $group->name ?>: <?= count($group->get()) ?>
+            <button class="showDeployments" style="margin-left: 10px;">
+                show
+            </button>
         </div>
+
+        <div class="modalContent" style="display: none; width: 100%;">
+            <h1>
+                <?= $group->name ?>
+            </h1>
+            <div style="width: 100%; text-align: center">
+                <button class="closeButton">
+                    close
+                </button>
+            </div>
+
+            <? foreach ($group->get() as $deployment): ?>
+                <div>
+                    <span style="color: #ccc; font-size: 0.79em; margin-right: 5px; display: inline-block;">
+                        <?= $view->dateFormatter->format(datetime: new \DateTime(datetime: $deployment->createdAt)) ?>
+                    </span>
+                    <?= $deployment->name ?>
+                </div>
+            <? endforeach ?>
+        </div>
+
         <div>
 
             <? foreach ($group->get() as $deployment): ?>
 
                 <?
-                if(++$count > 2) {
+                if(++$count > 3 ) {
                     break;
                 }
                 $style = ' style="';
@@ -41,36 +125,10 @@ declare(strict_types=1);
                     </span>
                     <?= $deployment->name ?>
 
-                    <style type="text/css">
 
-                    </style>
-                    <script>
-                        document.addEventListener('DOMContentLoaded', function () {
-                            const dialog = document.querySelector("dialog");
-                            const showButton = document.querySelector("dialog + button");
-                            const closeButton = document.querySelector("dialog button");
-                            showButton.addEventListener("click", () => {
-                                dialog.showModal();
-                            });
-                            closeButton.addEventListener("click", () => {
-                                dialog.close();
-                            });
-                        });
-                    </script>
-
-                    <dialog>
-                        <button autofocus>Close</button>
-                        <p>This modal dialog has a groovy backdrop!</p>
-                    </dialog>
-                    <button>Show the dialog</button>
                 </div>
             <? endforeach ?>
         </div>
     </div>
 <? endforeach ?>
 
-<script>
-    window.setTimeout(function() {
-        location.reload();
-    }, 20000);
-</script>
